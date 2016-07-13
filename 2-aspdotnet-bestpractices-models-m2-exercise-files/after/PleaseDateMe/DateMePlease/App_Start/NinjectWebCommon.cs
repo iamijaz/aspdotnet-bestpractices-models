bbed09048908bdb062d3b4043c632c799331da35
Ijaz Ahmed <Ijaz.Ahmed@dunnhumby.com>
2016-07-13 16:55:21 +0100

    added more projects

--- 2-aspdotnet-bestpractices-models-m2-exercise-files/after/PleaseDateMe/DateMePlease/App_Start/NinjectWebCommon.cs
+++ 2-aspdotnet-bestpractices-models-m2-exercise-files/after/PleaseDateMe/DateMePlease/App_Start/NinjectWebCommon.cs
@@ -3,65 +3,64 @@
 
 namespace DateMePlease.App_Start
 {
-  using System;
-  using System.Web;
-  using DateMePlease.Data;
-  using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-  using Ninject;
-  using Ninject.Web.Common;
+    using System;
+    using System.Web;
 
-  public static class NinjectWebCommon
-  {
-    private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
-    /// <summary>
-    /// Starts the application
-    /// </summary>
-    public static void Start()
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
     {
-      DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-      DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-      bootstrapper.Initialize(CreateKernel);
-    }
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
-    /// <summary>
-    /// Stops the application.
-    /// </summary>
-    public static void Stop()
-    {
-      bootstrapper.ShutDown();
-    }
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
-    /// <summary>
-    /// Creates the kernel that will manage your application.
-    /// </summary>
-    /// <returns>The created kernel.</returns>
-    private static IKernel CreateKernel()
-    {
-      var kernel = new StandardKernel();
-      try
-      {
-        kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-        kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
 
-        RegisterServices(kernel);
-        return kernel;
-      }
-      catch
-      {
-        kernel.Dispose();
-        throw;
-      }
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }        
     }
-
-    /// <summary>
-    /// Load your modules or register your services here!
-    /// </summary>
-    /// <param name="kernel">The kernel.</param>
-    private static void RegisterServices(IKernel kernel)
-    {
-      kernel.Bind<DateMePleaseContext>().ToSelf().InRequestScope();
-      kernel.Bind<IDateMePleaseRepository>().To<DateMePleaseRepository>();
-    }
-  }
 }
